cb1cbc9ecd4cb6030bac56a1f7aed12e9b50da9c
CaroNew <125692041+CaroNew@users.noreply.github.com>
2023-08-05 14:35:30 -0300

    agregada-mi-bitacora

--- angular-proyect/src/app/list-component/list-component.component.ts
+++ angular-proyect/src/app/list-component/list-component.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-
+/*mi customType*/
 type ProductType = {
   nombre:string,
   costo:number,
@@ -12,11 +12,12 @@ type ProductType = {
   styleUrls: ['./list-component.component.css']
 })
 export class ListComponentComponent {
-
+/*agregando productos*/
 productos: ProductType[] = [{ nombre: 'Python-Basic', costo: 135451 }, {nombre: 'C-Basic', costo:23544}, {nombre:'JavaScript', costo:13548}, { nombre: 'Web-design', costo: 132453 }, { nombre: 'Graphic-Design', costo: 11223 }, { nombre: 'Java-Basics', costo: 12313 }];
-  
+/*la lista esta oculta*/  
 showList: boolean = false;
-
+/*cuando llame a esta funcion showList 
+cambiará de estado a true y se mostrará la lista*/
 showTheList() {
   this.showList = !this.showList;
 }
